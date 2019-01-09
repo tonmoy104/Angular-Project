8f0ad1fc1bb2288ec906d50adb399d681b2d699d
Streamstech <bhowmik.tonmoy@gmail.com>
2019-01-09 13:42:42 +0600

    my first commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer=false;
+  allowMsg="Yes";
+  inputValue="Apps Server";
+  
+  constructor() {
+  setTimeout(()=>{this.allowNewServer=true;},2000)
+  
+  }
+
+  ngOnInit() {
+  }
+  onServerCreation(){
+  
+     this.allowMsg="Hi";
+  }
+  getServerName(evt:any){
+  console.log(evt);
+  this.inputValue=evt.target.value;
+     
+      
+  }
+
+}
